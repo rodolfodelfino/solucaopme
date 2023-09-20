80d02ce7eeb3719c3bedf2d00c5b8f48b091b806
rodolfodelfino <rodolfodelfino@hotmail.com>
2023-09-20 15:12:44 -0300

    commit

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 // Função para animar elementos quando entram na tela
-function animateOnScroll(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('slide-in'); // Adiciona a classe 'slide-in'
-            observer.unobserve(entry.target); // Interrompe a observação após a animação
-        }
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu ul");
+
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("active");
     });
-}
-
-// Cria um observador de interseção
-const observer = new IntersectionObserver(animateOnScroll, {
-    root: null, // O elemento raiz é o viewport
-    threshold: 0.3, // Define o valor de visibilidade necessário para disparar a animação
-});
-
-// Elementos que você deseja animar quando eles entrarem na tela
-const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
-
-elementsToAnimate.forEach(element => {
-    observer.observe(element); // Começa a observar cada elemento
 });
